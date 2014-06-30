dbc145830fd38dfad2da15ff04da7d2b5d93f3b7
gbachik <gbachik@gmail.com>
2014-06-30 17:31:26 -0600

    Stoppping for WWW

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(document).on('ready', function() {
   
+	var outerShell = $('<div class="outer-shell"></div>');
+	var innerShell = $('<div class="inner-shell"></div>');
+
+	$('.container').append(outerShell).append(innerShell);
 });

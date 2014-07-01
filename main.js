e2c83d45b2307f2870dcfcaf4b460648e1c5da71
gbachik <gbachik@gmail.com>
2014-07-01 10:43:55 -0600

    Added basic clock layouting

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ $(document).on('ready', function() {
   
 	var outerShell = $('<div class="outer-shell"></div>');
 	var innerShell = $('<div class="inner-shell"></div>');
+	var clockScreen = $('<div class="clock-screen"></div>');
 
-	$('.container').append(outerShell).append(innerShell);
+	$('.container').append(outerShell);
+	(innerShell).appendTo(outerShell);
+	(clockScreen).appendTo(innerShell);
 });

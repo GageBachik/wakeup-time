e261e002f457f40d45a47de10a6766bfd5a413e3
gbachik <gbachik@gmail.com>
2014-07-01 11:54:35 -0600

    Finished Clock

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 $(document).on('ready', function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	var hours = new Date().getHours();
   
 	var outerShell = $('<div class="outer-shell"></div>');
 	var innerShell = $('<div class="inner-shell"></div>');
+	var snooze = $('<div class="snooze"></div>');
 	var bottomAmLabel = $('<p class="bottom-am-label"> AM    60 70  90 110 140 170    KHz	</p>');
 	var bottomFmLabel = $('<p class="bottom-fm-label"> FM    89 92 96  102 106 108    MHz	</p>');
 	var clockScreen = $('<div class="clock-screen"></div>');
@@ -15,12 +19,11 @@ $(document).on('ready', function() {
 	(bottomAmLabel).appendTo(innerShell);
 	(bottomFmLabel).appendTo(innerShell);
 	(clockText).appendTo(innerShell);
+	(snooze).appendTo(innerShell);
+	(snooze.clone()).appendTo(innerShell);
+	$(".clock-text").html((( hours < 10 ? "0" : "" ) + hours) + ":" + (( minutes < 10 ? "0" : "" ) + minutes));
 
 	setInterval( function() {
-		// Create a newDate() object and extract the minutes of the current time on the visitor's
-		var minutes = new Date().getMinutes();
-		var hours = new Date().getHours();
-		// Add a leading zero to the minutes value
 		$(".clock-text").html((( hours < 10 ? "0" : "" ) + hours) + ":" + (( minutes < 10 ? "0" : "" ) + minutes));
 	},1000);
 	
